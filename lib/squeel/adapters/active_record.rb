7d4fca19a0aa2aa6e688e381fd4d2c822c79921c
Daniel Rhodes <rhodes.daniel@gmail.com>
2014-12-20 14:46:00 +0100

    Update active_record.rb

--- lib/squeel/adapters/active_record.rb
+++ lib/squeel/adapters/active_record.rb
@@ -1,5 +1,5 @@
 case ActiveRecord::VERSION::MAJOR
-when 3, 4, 5
+when 3, 4
   ActiveRecord::Relation.send :include, Squeel::Nodes::Aliasing
   require 'squeel/adapters/active_record/base_extensions'
 
